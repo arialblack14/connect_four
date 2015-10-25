ce1572d137a7c4ee360e69c8e484f468d6c8aa08
arialblack14 <tsirasalexis@yahoo.gr>
2015-10-25 18:25:05 +0200

    Added checker class and specs

---
+++ spec/spec_checker.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/checker.rb'
+require_relative '../spec/spec_helper'
+require 'rspec'
+
+describe Checker do
+  let(:checker) { Checker.new }
+  it "when one's player's turn drop one checker down the grid" do
+    checker
+  end
+end
