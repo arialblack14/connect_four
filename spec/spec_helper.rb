6c0087bdf8c0ba9e688892e616d84750a23adb6c
arialblack14 <tsirasalexis@yahoo.gr>
2015-10-25 16:37:20 +0200

    Configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
-RSpec.describe Board do
-  expect(Board.new).to eq(board)
+RSpec.configure do |config|
+	# Use color in STDOUT
+	config.color = true
+
+	# Use color in pagers and files too
+	config.tty = true
+
+	# Use the specified formatter
+	config.formatter = :documentation
 end
